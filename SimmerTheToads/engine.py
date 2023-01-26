bae84e8ed975ecd0066eadbb97e3ef8575fdb9f4
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2023-01-26 08:32:45 -0700

    Add option to reorder by specific feature

--- SimmerTheToads/engine.py
+++ SimmerTheToads/engine.py
@@ -1,6 +1,6 @@
-"""TODO."""
+"""Primary playlist manipulation module."""
 import os
-from pprint import pprint
+from pathlib import Path
 from typing import Optional
 
 import matplotlib.pyplot as plt
@@ -11,6 +11,16 @@ from spotipy.client import Spotify
 # pd.set_option("display.max_rows", None)
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", None)
+
+
+def grouper(iterable, n):
+    """Collect data into non-overlapping fixed-length chunks or blocks.
+
+    Adapted from the more-itertools recipes section:
+    https://docs.python.org/3/library/itertools.html#itertools-recipes
+    """
+    args = [iter(iterable)] * n
+    return zip(*args)
 
 
 class Track:
@@ -84,26 +94,46 @@ class Playlist:
         "track",
     ]
 
-    def __init__(self, spotify, id):
+    def __init__(self, spotify: Spotify, id: str):
         self.id = id
         self.metadata = spotify.playlist(id)
 
         # Retrieve all the tracks within the playlist
-        track_data = spotify.user_playlist_tracks(playlist_id=self.id)
-        track_ids = [i["track"]["id"] for i in track_data["items"]]
+        # Handle the pagination
+        track_items = []
+        result = spotify.user_playlist_tracks(playlist_id=self.id)
+        track_items.extend(result["items"])
+        while result["next"]:
+            result = spotify.next(result)
+            track_items.extend(result["items"])
+
+        track_ids = [i["track"]["id"] for i in track_items]
 
         # Get all the features of each track within the playlist.
         # Do this outside of the Track class to leverage the batched API.
-        features = spotify.audio_features(track_ids)
+        features = []
+        for chunk in grouper(track_ids, 100):
+            features.extend(spotify.audio_features(list(chunk)))
 
         rows = []
-        for metadata, features in zip(track_data["items"], features):
+        for metadata, features in zip(track_items, features):
             t = Track(spotify, metadata["track"], features)
             rows.append(t.features)
 
         self.df = pd.DataFrame(rows, columns=self.FEATURE_COLUMNS)
 
-    def plot(self):
+    def to_disk(self, path: Path):
+        """Dump the metadata to a JSON file on disk."""
+        # Drop the track object reference
+        serializeable_df = self.df.drop(["track"], axis=1)
+        serializeable_df.to_json(path, orient="records", indent=4)
+
+    def reorder_by_feature(self, feature):
+        """Reorder the playlist by a single feature."""
+        self.df = self.df.sort_values(by=feature)
+        self.df = self.df.reset_index()
+
+    def plot(self, fmt_title=None):
         """Show some simple statistics about this playlist."""
         nrows = 4
         ncols = 3
@@ -142,20 +172,23 @@ class Playlist:
                 ax=distplots_axes[i % (ncols + 1)][i // nrows],
             )
 
-        lineplots_fig.suptitle(f"{self.metadata['name']}")
+        if fmt_title is None:
+            title = f"{self.metadata['name']}"
+        else:
+            title = fmt_title.format(name=self.metadata["name"])
+
+        lineplots_fig.suptitle(title)
         lineplots_fig.tight_layout()
 
-        distplots_fig.suptitle(f"{self.metadata['name']}")
+        distplots_fig.suptitle(title)
         distplots_fig.tight_layout()
-        plt.show()
 
 
 def simmer_playlist(
     spotify: Spotify,
     playlist_id: str,
 ) -> list[Track]:
-    """
-    Reorder / add songs to playlist for simmering.
+    """Reorder / add songs to playlist for simmering.
 
     This is the main entrypoint to the playlist processing engine. It queries a
     playlist for all the required information about a playlist, then reorders
@@ -164,12 +197,18 @@ def simmer_playlist(
 
     :param spotify: Current spotify OAuth session
     :param playlist_id: ID of the playlist to simmer.
+
     """
-    # TODO: This will need to handle the pagination eventually
-    #       Currently, this will only handle the first 100 songs.
-    #       Also, beware the rate limit...
     p = Playlist(spotify, playlist_id)
-    p.plot()
+    p.to_disk(Path("playlist.json"))
+    p.plot(fmt_title="{name}: Original")
+
+    reorder_feature = "acousticness"
+    p.reorder_by_feature(reorder_feature)
+
+    p.plot(fmt_title="{{name}}: Reordered by {feature}".format(feature=reorder_feature))
+
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -189,7 +228,6 @@ if __name__ == "__main__":
         "playlist-modify-private",
         "playlist-modify-public",
     ]
-
     auth_manager = SpotifyOAuth(
         scope=" ".join(OAUTH_SCOPES),
         client_id=CLIENT_ID,
@@ -200,6 +238,7 @@ if __name__ == "__main__":
 
     spotify = spotipy.Spotify(auth_manager=auth_manager)
 
+    # Just an easy way to quickly switch between playlists for testing.
     playlists = {
         "Abbey Road": "2CPPiUTAtfvaeAEyWApOSE",
         "Fiddler on the roof": "3engsNaAr6Q9cTT85gKCGd",
@@ -208,7 +247,6 @@ if __name__ == "__main__":
         "The Big Bach": "4OFbt8ZZOhE0oZcC4GGBMO",
         "godawful amalgamation": "0KOMGaR3mgeymmUBoN0ZlQ",
         "close but no cigar": "78C62xSuql1V2jwMvonI4N",
-        "manic pixel dream girl complex": "3ZL5feDBYxJSWR6zg7EDeu",
+        "manic pixie dream girl complex": "3ZL5feDBYxJSWR6zg7EDeu",
     }
-
-    simmer_playlist(spotify, playlists["manic pixel dream girl complex"])
+    simmer_playlist(spotify, playlists["The Big Bach"])
