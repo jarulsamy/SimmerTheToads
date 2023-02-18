14e2890619987411672bc52581c89359b5250f6a
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2023-02-17 19:14:00 -0700

    Add more react flask communication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Flask from "./Flask";
+import axios from "axios";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Simmer the Toads, now with more JS!</p>
+        <Flask />
       </header>
     </div>
   );
