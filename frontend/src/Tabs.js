839401819d061707e6b43c1fceb27e0d6ab53388
Natalie Foss <nat@natalies-mbp-2.uwyo.edu>
2023-03-24 14:39:55 -0600

    New MUI tabs

--- frontend/src/Tabs.js
+++ frontend/src/Tabs.js
@@ -1,36 +1,79 @@
-import "./css/Tabs.css";
+// import "./css/Tabs.css";
+import React , { useState } from "react";
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
 import About from "./About";
 import PlaylistCards from "./PlaylistsView";
-import React from "react";
 import SongForm from "./song_form";
 import Spotify from "./Flask";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
-function CreateTabs() {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <Tabs className={"Tabs"}>
-      <TabList className={"TabList"}>
-        <Tab className={"Tab"}>Create Playlist</Tab>
-        <Tab className={"Tab"}>Spotify</Tab>
-        <Tab className={"Tab"}>About</Tab>
-        <Tab className={"Tab"}>Simmer Existing Playlist</Tab>
-      </TabList>
-
-      <TabPanel className={"TabPanel"}>
-        <SongForm />
-      </TabPanel>
-      <TabPanel className={"TabPanel"}>
-        <h1>Spotify related things...</h1>
-        <Spotify />
-      </TabPanel>
-      <TabPanel className={"TabPanel"}>
-        <About />
-      </TabPanel>
-      <TabPanel className={"TabPanel"}>
-        <PlaylistCards />
-      </TabPanel>
-    </Tabs>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 4 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
   );
 }
 
-export default CreateTabs;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function CreateTabs() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Create Playlist" {...a11yProps(0)} />
+          <Tab label="Spotify" {...a11yProps(1)} />
+          <Tab label="About" {...a11yProps(2)} />
+          <Tab label="Simmer Existing Playlist" {...a11yProps(3)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <SongForm />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <h1>Spotify related things...</h1>
+        <Spotify />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <About />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <PlaylistCards />
+      </TabPanel>
+    </Box>
+  );
+}
