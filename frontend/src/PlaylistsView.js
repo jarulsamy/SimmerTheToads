09888915c8a469784e5fea4d896ec2548e68485f
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2023-04-10 12:23:54 -0600

    Fix scrolling

--- frontend/src/PlaylistsView.js
+++ frontend/src/PlaylistsView.js
@@ -14,27 +14,25 @@ class PlaylistCardsContainer extends React.Component {
     this.state = {
       toBeSimmered: [],
       simmerQueued: false,
-      loading: false
+      loading: false,
     };
 
     this.simmerPlaylist = this.simmerPlaylist.bind(this);
     this.selectedCard = this.selectedCard.bind(this);
   }
 
-
   selectedCard(isNew, newID, newName) {
     if (isNew) {
-      console.log("adding to the list: "+newName)
-      this.setState(prevState => ({
-        toBeSimmered: [...prevState.toBeSimmered, {id: newID, name: newName}]
-      }))
-    }
-    else {
-      console.log("removing from the list: "+newName);
+      console.log("adding to the list: " + newName);
+      this.setState((prevState) => ({
+        toBeSimmered: [...prevState.toBeSimmered, { id: newID, name: newName }],
+      }));
+    } else {
+      console.log("removing from the list: " + newName);
       var temp = this.state.toBeSimmered;
-      var index = temp.indexOf({id: newID, name: newName});
+      var index = temp.indexOf({ id: newID, name: newName });
       temp.splice(index, 1);
-      this.setState({toBeSimmered: temp});
+      this.setState({ toBeSimmered: temp });
     }
   }
 
@@ -42,46 +40,54 @@ class PlaylistCardsContainer extends React.Component {
     // this function is called when the user clicked one of the options in the dropdown menu
     if (this.state.toBeSimmered.length > 0) {
       // which simmering method
-      var evaluator = (simmerMethod === "Simmer") ? "clustering" : ((simmerMethod === "Bake") ? "tsp" : "chaos");
-      console.log("Simmering with method: "+evaluator);
-      this.setState({loading: true});
-      this.state.toBeSimmered.forEach(playlist => {
-        console.log("playlsit id: "+playlist.id+", playlist name: "+playlist.name)
-        this.setState({setSimmerQueued: true});
+      var evaluator =
+        simmerMethod === "Simmer"
+          ? "clustering"
+          : simmerMethod === "Bake"
+          ? "tsp"
+          : "chaos";
+      console.log("Simmering with method: " + evaluator);
+      this.setState({ loading: true });
+      this.state.toBeSimmered.forEach((playlist) => {
+        console.log(
+          "playlsit id: " + playlist.id + ", playlist name: " + playlist.name
+        );
+        this.setState({ setSimmerQueued: true });
         // setAlert("info", `Started simmering ${playlist.name}. Please wait...`);
         APIService.simmeredPlaylistTracks(playlist.id, true, evaluator).then(
           (resp) => {
             console.log(`Simmered: ${playlist.id}`);
             // setAlert("success", `Successfully simmered: ${playlist.name}`);
-            this.setState({setSimmerQueued: false});
-            this.setState({loading: false});
+            this.setState({ setSimmerQueued: false });
+            this.setState({ loading: false });
           },
           (error) => {
             console.error(error);
             // setAlert("error", "Something went wrong. Please try again.");
-            this.setState({setSimmerQueued: false});
-            this.setState({loading: false});
+            this.setState({ setSimmerQueued: false });
+            this.setState({ loading: false });
           }
         );
-      })
+      });
+    } else {
+      console.log("no playlists to simmer");
     }
-    else {console.log("no playlists to simmer")}
   }
 
   render() {
     return (
       <Container>
-        <div style={{overflowY: 'scroll', height: '80vh'}}>
-          <PlaylistCards selectedCard={this.selectedCard}/>
-
+        <div style={{ overflowY: "scroll", height: "80vh" }}>
+          <PlaylistCards selectedCard={this.selectedCard} />
         </div>
-        <Box 
+        <Box
           m={1}
           //margin
           display="flex"
           justifyContent="flex-end"
-          alignItems="flex-end">
-            <SimmerMenu onChange={this.simmerPlaylist}/>
+          alignItems="flex-end"
+        >
+          <SimmerMenu onChange={this.simmerPlaylist} />
         </Box>
         {this.state.loading ? <SimpleBackdrop /> : <></>}
       </Container>
@@ -89,19 +95,24 @@ class PlaylistCardsContainer extends React.Component {
   }
 }
 
-
 function PlaylistCards(props) {
   const [playlists, setPlaylists] = React.useState([]);
-  
+
   React.useEffect(() => {
     APIService.getPlaylists().then((resp) => {
       setPlaylists(resp.data.items);
     });
-  }, [])
-  
+  }, []);
+
   return (
-    <Box >
-      <Grid container rowSpacing={4} spacing={4} justifyContent="center">
+    <Box>
+      <Grid
+        container
+        rowSpacing={4}
+        spacing={4}
+        justifyContent="center"
+        style={{ overflow: "auto" }}
+      >
         {playlists.map((p) => {
           return (
             <Grid item key={p.id}>
@@ -112,7 +123,7 @@ function PlaylistCards(props) {
                   description={p.description}
                   images={p.images}
                   selectedCard={props.selectedCard}
-                  />
+                />
               </Paper>
             </Grid>
           );
@@ -123,5 +134,3 @@ function PlaylistCards(props) {
 }
 
 export default PlaylistCardsContainer;
-
-
